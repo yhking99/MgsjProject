88490a62b4597505add8573ecee8c182dc4cb1da
Auden Kim <sasameyuki03@naver.com>
2023-02-22 13:15:12 +0900

    2023-02-22 (재국) 토큰이슈 해결

--- src/main/java/com/project/board/controller/BoardController.java
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -1,8 +1,5 @@
 package com.project.board.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.board.domain.BoardDTO;
 import com.project.board.service.BoardServiceImpl;
