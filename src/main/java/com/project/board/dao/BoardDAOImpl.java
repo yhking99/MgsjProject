4145657cfc503f2431456cceb504ed1115f611df
Jinny0827 <116527044+Jinny0827@users.noreply.github.com>
2023-02-22 11:25:08 +0900

    23.02.22 (원진) 게시판 CRD 구현, U는 차후에

--- src/main/java/com/project/board/dao/BoardDAOImpl.java
+++ src/main/java/com/project/board/dao/BoardDAOImpl.java
@@ -1,5 +1,5 @@
 package com.project.board.dao;
- 
+
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,30 +10,67 @@ import com.project.board.domain.BoardDTO;
 
 @Repository
 public class BoardDAOImpl implements BoardDAO {
-  
-  private static final Logger logger = LoggerFactory.getLogger(BoardDAOImpl.class);
-	
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardDAOImpl.class);
+
 	@Autowired
 	private SqlSession sqlSession;
-	
+
 	private static final String NAME_SPACE = "mappers.boardMapper";
-	
-	// 공지사항 등록
+
+	// 공지 게시글 등록
 	@Override
 	public void boardWriteOp(BoardDTO boardDTO) throws Exception {
-		
-		logger.info("BoardDAOImpl에서 운영진 게시글 등록하기 시작");
-		
+
+		logger.info("BoardDAOImpl에서 공지 게시글 등록하기 시작");
+
 		sqlSession.insert(NAME_SPACE + ".boardWriteOp", boardDTO);
 	}
-	
-	// 일반게시판 등록
+
+	// 이용자 게시글 등록
 	@Override
 	public void boardWrite(BoardDTO boardDTO) throws Exception {
-		
+
 		logger.info("BoardDAOImpl에서 이용자 게시글 등록하기 시작");
-		
+
 		sqlSession.insert(NAME_SPACE + ".boardWrite", boardDTO);
+	}
+	// 공지 게시글 삭제
+
+	@Override
+	public void boardDeleteOp(int bno) throws Exception {
+		
+		logger.info("BoardDAOImpl에서 공지 게시글 삭제하기 시작");
+
+		sqlSession.delete(NAME_SPACE + ".boardDeleteOp", bno);
+	}
+
+	// 이용자 게시글 삭제
+	@Override
+	public void boardDelete(int bno) throws Exception {
+		
+		logger.info("BoardDAOImpl에서 이용자 게시글 삭제하기 시작");
+
+		sqlSession.delete(NAME_SPACE + ".boardDelete", bno);
+	}
+	
+	//공지 게시글 상세 조회
+	@Override
+	public BoardDTO boardDetailOp(int bno) throws Exception {
+
+		logger.info("BoardDAOImpl에서 공지 게시글 상세 조회하기 시작");
+		
+		return sqlSession.selectOne(NAME_SPACE + ".boardDetailOp", bno);
+	}
+	
+	//이용자 게시글 상세 조회
+	@Override
+	public BoardDTO boardDetail(int bno) throws Exception {
+		
+		logger.info("BoardDAOImpl에서 이용자 게시글 상세 조회하기 시작");
+		
+		return sqlSession.selectOne(NAME_SPACE + ".boardDetail", bno);
+		
 	}
 
 }
