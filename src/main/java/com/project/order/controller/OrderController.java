03b84486ed3a57ce0a82636b02b8d6395a66ef6c
Jinny0827 <116527044+Jinny0827@users.noreply.github.com>
2023-02-23 17:06:23 +0900

    23.02.23 (원진) 주문 정보 구현 및 간이 실현파일(orderList.jsp)

--- src/main/java/com/project/order/controller/OrderController.java
+++ src/main/java/com/project/order/controller/OrderController.java
@@ -1,9 +1,91 @@
 package com.project.order.controller;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.order.domain.OrderDTO;
+import com.project.order.service.OrderService;
 
 @Controller
 public class OrderController {
 
+	private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+	
+	@Autowired
+	private OrderService orderService;
+	
+	
+	// 주문 작성 페이지
+	@RequestMapping(value = "/order/orderWirtePage", method = RequestMethod.GET)
+	public String orderWirtePage() throws Exception {
+		
+		logger.info("주문 작성 페이지 orderWirtePage - Controller");
+		
+	return "/order/orderWirtePage";
+		
+	}
+	// 주문 등록
+	@RequestMapping(value = "/order/orderWrite", method = RequestMethod.POST)
+	public String orderWrite(OrderDTO orderDTO) throws Exception {
+		
+		logger.info("주문 작성 orderWrite - Controller");
+		
+		orderService.orderWrite(orderDTO);
+		
+		return "redirect:/order/main";
+	}
+	// 주문 수정
+	@RequestMapping(value = "/order/orderUpdate", method = RequestMethod.POST)
+	public String orderUpdate(OrderDTO orderDTO) throws Exception {
+		
+		logger.info("주문 수정 orderUpdate - Controller");
+		
+		orderService.orderUpdate(orderDTO);
+		
+		return "redirect:/order/main";
+	}
+	
+	// 주문 삭제
+	@RequestMapping(value = "/order/orderDelete", method = RequestMethod.POST)
+	public String orderDelete(int orderNum) throws Exception {
+		
+		logger.info("주문 삭제 orderDelete - Controller");
+		
+		orderService.orderDelete(orderNum);
+		
+		return "redirect:/order/main";
+	}
+	
+	// 주문 조회
+	@RequestMapping(value = "/order/orderView", method = RequestMethod.GET)
+	public void orderView(Model model, OrderDTO orderDTO, int orderNum) throws Exception {
+		
+		logger.info("주문 조회 orderView - Controller");
+		
+		orderService.orderView(orderNum);
+		
+		model.addAttribute("orderDTO", orderDTO);
+	}
+	
+	// 주문 목록
+	@RequestMapping(value = "/order/orderList", method = RequestMethod.GET)
+	public void orderList(Model model) throws Exception {
+		
+		logger.info(" 주문 목록 orderList - Controller");
+		
+		List<OrderDTO> orderList = orderService.orderList();
+		
+		logger.info(" 주문 목록 ==> " + orderList);
+		
+		model.addAttribute("orderList", orderList);
+		
+	}
 	
 }
--- src/main/java/com/project/order/dao/OrderDAOImpl.java
+++ src/main/java/com/project/order/dao/OrderDAOImpl.java
@@ -1,8 +1,64 @@
 package com.project.order.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.project.order.domain.OrderDTO;
 
 @Repository
 public class OrderDAOImpl implements OrderDAO {
 
+	private static final Logger logger = LoggerFactory.getLogger(OrderDAOImpl.class);
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	private static final String NAME_SPACE = "mappers.orderMapper";
+	
+	// 주문 등록
+	@Override
+	public void orderWrite(OrderDTO orderDTO) throws Exception {
+		
+		logger.info("주문 등록 orderWrite - DAO");
+		
+		sqlSession.insert(NAME_SPACE + ".orderWrite", orderDTO);
+	}
+	// 주문 조회
+	@Override
+	public OrderDTO orderView(int orderNum) throws Exception {
+		
+		logger.info("주문 조회 orderView - DAO");
+		
+		
+	return sqlSession.selectOne(NAME_SPACE + ".orderView", orderNum);
+	}
+	// 주문 수정
+	@Override
+	public void orderUpdate(OrderDTO orderDTO) throws Exception {
+		
+		logger.info("주문 수정 orderUpdate - DAO");
+		
+		sqlSession.update(NAME_SPACE + ".orderUpdate", orderDTO);
+	}
+	// 주문 삭제
+	@Override
+	public void orderDelete(int orderNum) throws Exception {
+	
+		logger.info("주문 삭제 orderDelete - DAO");
+		
+		sqlSession.delete(NAME_SPACE + ".orderDelete", orderNum);
+	}
+	// 주문 목록
+	@Override
+	public List<OrderDTO> orderList() throws Exception {
+		
+		logger.info("주문 목록 orderList - DAO");
+		
+		return sqlSession.selectList(NAME_SPACE + ".orderList");
+	}
 }
