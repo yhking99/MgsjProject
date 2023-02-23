13438937103e16313e9f3ac80da6ba0aa79e5b4d
Jinny0827 <116527044+Jinny0827@users.noreply.github.com>
2023-02-23 11:38:59 +0900

    23.02.23 (원진) 상품 구현, 상품 구현 간이 앞단(productList.jsp)

--- src/main/java/com/project/product/service/ProductService.java
+++ src/main/java/com/project/product/service/ProductService.java
@@ -1,23 +1,23 @@
-package com.project.product.dao;
+package com.project.product.service;
 
 import java.util.List;
 
 import com.project.product.domain.ProductDTO;
 
-public interface ProductDAO {
-	
+public interface ProductService {
+
 	// 상품 게시글 등록하기
 	public void productWrite(ProductDTO productDTO) throws Exception;
 	
-	// 상품 게시글 삭제하기
-	public void productDelete(int pno) throws Exception;
-	
-	// 상품 게시글 수정하기
+	// 상품 게시글 수정
 	public void productUpdate(ProductDTO productDTO) throws Exception;
 	
-	// 상품 게시글 상세조회
-	public ProductDTO productDetail(int pno) throws Exception;
+	// 상품 게시글 삭제
+	public void productDelete(int pno) throws Exception;
 	
 	// 상품 게시글 목록
 	public List<ProductDTO> productList() throws Exception;
+	
+	// 상품 게시글 상세조회
+	public ProductDTO productDetail(int pno) throws Exception;
 }
--- src/main/java/com/project/product/service/ProductServiceImpl.java
+++ src/main/java/com/project/product/service/ProductServiceImpl.java
@@ -1,8 +1,65 @@
 package com.project.product.service;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class ProductServiceImpl implements ProductSerivce {
+import com.project.product.dao.ProductDAO;
+import com.project.product.domain.ProductDTO;
 
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	private static final Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
+	
+	
+	@Autowired
+	private ProductDAO productDAO;
+
+	//상품 게시글 등록하기
+	@Override
+	public void productWrite(ProductDTO productDTO) throws Exception {
+
+		logger.info("상품 게시글 등록하기 productWrite - Service");
+		
+		productDAO.productWrite(productDTO);
+	}
+	//상품 게시글 삭제하기
+	@Override
+	public void productDelete(int pno) throws Exception {
+
+		logger.info("상품 게시글 삭제하기 productDelete - Service");
+		
+		productDAO.productDelete(pno);
+	}
+	
+	//상품 게시글 수정하기
+	@Override
+	public void productUpdate(ProductDTO productDTO) throws Exception {
+
+		logger.info("상품 게시글 수정하기 productUpdate - Service");
+		
+		productDAO.productUpdate(productDTO);
+	}
+	
+	//상품 게시글 상세 조회
+	@Override
+	public ProductDTO productDetail(int pno) throws Exception {
+
+		logger.info("상품 게시글 상세조회 productDetail - Service");
+		
+		return productDAO.productDetail(pno);
+	}
+	
+	//상품 게시글 목록
+	@Override
+	public List<ProductDTO> productList() throws Exception {
+
+		logger.info("상품 게시글 목록 조회 productList - Service");
+		
+		return productDAO.productList();
+	}
 }
