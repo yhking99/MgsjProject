a2e836e7573bea7fc0d72f3d748f590b26f7f812
Auden Kim <sasameyuki03@naver.com>
2023-02-27 16:17:09 +0900

    2023-02-27 (재국) 수정사항 테스트

--- src/main/java/com/project/product/domain/CategoryDetailDTO.java
+++ src/main/java/com/project/product/domain/CategoryDetailDTO.java
@@ -1,34 +1,38 @@
 package com.project.product.domain;
- 
+
 public class CategoryDetailDTO {
 
 	private int cdno;
 	private int cno;
 	private String cdTitle;
-	
+
 	public int getCdno() {
 		return cdno;
 	}
+
 	public void setCdno(int cdno) {
 		this.cdno = cdno;
 	}
+
 	public int getCno() {
 		return cno;
 	}
+
 	public void setCno(int cno) {
 		this.cno = cno;
 	}
+
 	public String getCdTitle() {
 		return cdTitle;
 	}
+
 	public void setCdTitle(String cdTitle) {
 		this.cdTitle = cdTitle;
 	}
+
 	@Override
 	public String toString() {
 		return "CategoryDetailDTO [cdno=" + cdno + ", cno=" + cno + ", cdTitle=" + cdTitle + "]";
 	}
-	
-	
-	
+
 }
